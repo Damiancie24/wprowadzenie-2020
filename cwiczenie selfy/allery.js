b7ce43d8fa58c0db248a0900c67bd0aa6949944a
Damiancie24 <d.cieszynski@microsoft.wsei.edu.pl>
2020-05-02 20:35:30 +0200

    java script

---
+++ cwiczenie selfy/allery.js
@@ -0,0 +1,38 @@
+// console.log("czesc");
+// // pobranie elementu po klasie css
+// let divtext = document.querySelector('.text')
+// // pobranie elementu po id css
+// // let divtext = document.querySelector('#heder')
+
+// // divtext.innerHTML += "czesc damian!"
+// // divtext.innertext = "witam cie damian!"
+
+// let subhedder = document.querySelector('.aleo');
+
+// subhedder.style.background = '#aaaaaa';
+// subhedder.style.paddingRight = '50px'; 
+// // dodania clasy
+// let hedder4 = document.querySelector('h4')
+// hedder4.classList.add('bigger')
+// hedder4.classList.add('animate')
+// // usuwanie class
+// hedder4.classList.remove('bigger')
+
+// // przelaczanie i sprawdzanie czy jest włączona clasa (jesli jest to wyłączy i na odwrot)
+// hedder4.classList.toggle('bigger')
+
+
+// let button1 = document.querySelector('.nieklik');
+// button1.addEventListener('click', onButton1Clik)
+
+// function onButton1Clik(event){
+//     // console.log(event)
+// hedder4.classList.toggle('bigger')
+// hedder4.classList.add('animate')
+
+// }
+
+
+
+
+
